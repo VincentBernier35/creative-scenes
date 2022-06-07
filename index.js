097c56989bad39874d54827593b2a0fd7c534049
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-06-07 21:18:17 +0200

    config server on index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//hello world !
+require("dotenv").config;
+
+const express = require("express");
+const app = express();
+
+// static files
+app.use(express.static("public"));
+
+// router
+const router = require("./app/router");
+
+app.use(router);
+
+// server
+const PORT = process.env.PORT ?? 4000;
+app.listen(PORT, () => {
+  console.log(`Running server on : http://localhost:${PORT}`);
+});
