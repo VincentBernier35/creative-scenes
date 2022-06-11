35136920a1a9539513f34c6935dec88c40132ed4
VincentBernier35 <vincent.bernier@hotmail.fr>
2022-06-11 13:53:14 +0200

    :construction: first route get + controller

---
+++ app/controller/controller.js
@@ -0,0 +1,7 @@
+const sceneController = {
+  getOne(_, res) {
+    console.log("log from sceneController.getOne");
+  }
+};
+
+module.exports = sceneController;
--- app/router.js
+++ app/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const sceneController = require("./controller/controller");
+
+router.get("/api/scene", sceneController.getOne);
 
 
 module.exports = router;
